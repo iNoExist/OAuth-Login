67e93d8f786a7e42101ca7dadbf2f02c4f293cb9
g60080zu <09zu01@gmail.com>
2025-04-12 20:00:00 +0100

    Log out

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
-import { auth, GoogleAuthProvider, signInWithPopup } from './firebase-config.js';
+import { auth, GoogleAuthProvider, signInWithPopup, signOut } from './firebase-config.js';
 
 const googleProvider = new GoogleAuthProvider();
-const app = document.getElementById("app");
+const text = document.getElementById("text");
 const loginBtn = document.getElementById("btn-siwg");
 const logoutBtn = document.getElementById("btn-so");
+var username;
 
 const loginSIWG = async () => {
     signInWithPopup(auth, googleProvider)
     .then((result) => {
-      // User is signed in
+
       const user = result.user;
+      username = user.displayName || user.email;
       console.log("User signed in:", user);
-      app.innerHTML = `<h3>Hi ${user.displayName || user.email} 👋</h3>`;
+
+      text.innerHTML = `<h3>Hi ${username} 👋</h3>`;
+      loginBtn.style.display = "none";
+      logoutBtn.style.display = "flex";
+
     })
     .catch((error) => {
       console.error("Error during Google Sign-In:", error);
     });
 };
 
+const logoutSOWG = async () => {
+    try {
+        await signOut(auth);  // Sign out the user
+        console.log("User signed out");
+  
+      // Clear the displayed user info and update the UI
+      text.innerHTML = `<h3>Goodbye ${username}! 👋</h3>`;
+  
+      // Show the login button and hide the logout button
+      loginBtn.style.display = "flex";
+      logoutBtn.style.display = "none";
+    } catch (error) {
+      console.error("Error during Sign-Out:", error);
+    }
+  };
+
 
 loginBtn.addEventListener("click", loginSIWG);
+logoutBtn.addEventListener("click", logoutSOWG);
