1806f18aebeb051de5c1ee8b3de187660bbeb8e7
g60080zu <09zu01@gmail.com>
2025-04-12 17:37:08 +0100

    Initial frontend

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("main.js is running!");
+
+const app = document.getElementById("app");
+const loginBtn = document.getElementById("btn-siwg");
+
+const loginSIWG = async () => {};
+    
+
+loginBtn.addEventListener("click", loginSIWG);
