0e079704ee5f74720ec9b59b70935309bceabb99
g60080zu <09zu01@gmail.com>
2025-04-12 19:34:57 +0100

    Sign In

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-console.log("main.js is running!");
+import { auth, GoogleAuthProvider, signInWithPopup } from './firebase-config.js';
 
+const googleProvider = new GoogleAuthProvider();
 const app = document.getElementById("app");
 const loginBtn = document.getElementById("btn-siwg");
+const logoutBtn = document.getElementById("btn-so");
 
-const loginSIWG = async () => {};
-    
+const loginSIWG = async () => {
+    signInWithPopup(auth, googleProvider)
+    .then((result) => {
+      // User is signed in
+      const user = result.user;
+      console.log("User signed in:", user);
+      app.innerHTML = `<h3>Hi ${user.displayName || user.email} 👋</h3>`;
+    })
+    .catch((error) => {
+      console.error("Error during Google Sign-In:", error);
+    });
+};
+
 
 loginBtn.addEventListener("click", loginSIWG);
